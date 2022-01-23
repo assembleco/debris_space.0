57817e090e9d6322c6f7e0937b0afeb4b494724f
grace <g@assembled.app>
2022-01-23 14:55:48 -0500

    [base] Include `g3nd-0.6@4147e66` engine & depends

---
+++ depends.sh
@@ -0,0 +1,9 @@
+# Fedora:> \
+sudo dnf -y install xorg-x11-proto-devel mesa-libGL mesa-libGL-devel openal-soft openal-soft-devel libvorbis libvorbis-devel glfw-devel libXi-devel libXxf86vm-devel
+
+# Ubuntu/Debian:> sudo apt-get install xorg-dev libgl1-mesa-dev libopenal1 libopenal-dev libvorbis0a libvorbis-dev libvorbisfile3
+# CentOS 7:> sudo yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+# Arch:> sudo pacman -S base-devel xorg-server mesa openal libvorbis
+# Void:> sudo xbps-install git xorg-server-devel base-devel libvorbis-devel libvorbis libXxf86vm-devel libXcursor-devel libXrandr-devel libXinerama-devel libopenal libopenal-devel libglvnd-devel# Ubuntu/Debian:> sudo apt-get install xorg-dev libgl1-mesa-dev libopenal1 libopenal-dev libvorbis0a libvorbis-dev libvorbisfile3
+# Mac OS:> brew install libvorbis openal-soft
+# Windows: See https://github.com/g3n/engine#windows
